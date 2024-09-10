6bcde6db64ce24a8a7bd5cac47540740cfb56e40
allisonhart18 <adhart@smu.edu>
2024-09-10 17:01:46 -0500

    first commit

---
+++ partical_engine/src/main/java/com/partical_engine_example/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Coder: Allison Hart
+ * Date 9/10/2024
+ * Description: Particle engine project
+ * 
+ */
+
+
+package com.partical_engine_example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
